271118845a78ded102b3a8503ba9f4a96ed620b0
Pontus Nydensten <pontusn@kth.se>
2017-04-11 12:53:10 +0200

    Fix bad merge of server start

--- server/server.js
+++ server/server.js
@@ -26,17 +26,15 @@ require('./init/logging')
 
 server.locals.secret = new Map()
 
-module.exports.run = function () {
-  server.start({
-    useSsl: config.useSsl,
-    pfx: config.ssl.pfx,
-    passphrase: config.ssl.passphrase,
-    key: config.ssl.key,
-    ca: config.ssl.ca,
-    cert: config.ssl.cert,
-    port: config.port,
-    logger: log
-  })
-}
+server.start({
+  useSsl: config.useSsl,
+  pfx: config.ssl.pfx,
+  passphrase: config.ssl.passphrase,
+  key: config.ssl.key,
+  ca: config.ssl.ca,
+  cert: config.ssl.cert,
+  port: config.port,
+  logger: log
+})
 
 module.exports = server
