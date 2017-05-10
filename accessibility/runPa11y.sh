0980e768a8649d1da87d85ad6bd0f7dc899ba67b
Simon Wikander <simplish.open@gmail.com>
2017-05-10 16:50:25 +0200

    Fixed pa11y script and fixed some a11y issues.

--- accessibility/runPa11y.sh
+++ accessibility/runPa11y.sh
@@ -23,10 +23,11 @@ mkdir $OUTPUT_DIR
 
 
 echo "Mirroring $TARGET into $TMP_DIR"
-wget -P $TMP_DIR \
+wget -P $TMP_DIR -e robots=off \
   --no-directories \
   --mirror \
   --adjust-extension \
+  --convert-links \
   --quiet \
   $TARGET
 
@@ -36,26 +37,31 @@ do
   FILE_PATH="file://$FILE"
   REPORT_OUTPUT=${OUTPUT_DIR}report_${FILE_NAME}.html
   SCREENSHOT_OUTPUT=${OUTPUT_DIR}report_${FILE_NAME}_screenshot.png
-  
+
   pa11y --reporter html \
-    --ignore "warning;notice" \
+    --level "warning" \
+    --ignore "notice" \
+    --ignore "WCAG2AA.Principle1.Guideline1_4.1_4_3.G18.Fail" \
+    --root-element "#main-content" \
     --standard $STANDARD \
     --screen-capture $SCREENSHOT_OUTPUT \
     $FILE_PATH > $REPORT_OUTPUT
+  PA11Y_EXIT=$?
 
-  if [ $? -eq 0 ]
+  if [ $PA11Y_EXIT -eq 0 ]
   then
+    echo "Pa11y OK for $FILE_NAME"
     rm $REPORT_OUTPUT
     rm $SCREENSHOT_OUTPUT
+    echo "Files removed"
   else
     EXIT_CODE=$(($EXIT_CODE+$?))
     echo "Pa11y failed with $FILE_NAME, output can be found at $REPORT_OUTPUT"
   fi
 done
 
-
 echo "Removing temporary dir for pa11y-tested page"
-##rm -rf $TMP_DIR
+#rm -rf $TMP_DIR
 
 if [ $EXIT_CODE -gt 0 ]
 then
