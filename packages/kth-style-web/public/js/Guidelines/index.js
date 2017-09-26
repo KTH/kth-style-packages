1b536aa56fceb621527b1e6fcec3e9dd4897176a
asalindstrom <asahu@kth.se>
2017-09-26 16:33:06 +0200

    Add information page to menu

--- packages/kth-style-web/public/js/Guidelines/index.js
+++ packages/kth-style-web/public/js/Guidelines/index.js
@@ -1,7 +1,9 @@
 import StartPage from './StartPage.jsx'
+import InformationPage from './InformationPage.jsx'
 
 const pages = [
   { path: "/", title: "Guidelines", header: "h2", component: StartPage},
+  { path: "/information", title: "Information", component: InformationPage}
 ]
 
 export default pages
