b58dbe36d7a870a925f09407c944e396ae29a7b9
asalindstrom <asahu@kth.se>
2017-09-28 17:05:14 +0200

    Update with page "Button or link"

--- packages/kth-style-web/public/js/Guidelines/index.js
+++ packages/kth-style-web/public/js/Guidelines/index.js
@@ -1,9 +1,11 @@
 import StartPage from './StartPage.jsx'
 import InformationPage from './InformationPage.jsx'
+import ButtonOrLinkPage from './ButtonOrLinkPage.jsx'
 
 const pages = [
   { path: "/", title: "Guidelines", header: "h2", component: StartPage},
   { path: "/information", title: "Information", component: InformationPage}
+  { path: "/buttonorlink", title: "Button or link", component: ButtonOrLinkPage}
 ]
 
 export default pages
