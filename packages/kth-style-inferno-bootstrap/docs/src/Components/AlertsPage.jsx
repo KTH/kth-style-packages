d8fd7a3e7badc0e0284ec65aa9809102be0d4258
asalindstrom <asahu@kth.se>
2017-10-05 13:53:59 +0200

    Updated with links and reorder elements

--- packages/kth-style-inferno-bootstrap/docs/src/Components/AlertsPage.jsx
+++ packages/kth-style-inferno-bootstrap/docs/src/Components/AlertsPage.jsx
@@ -6,12 +6,170 @@ import Alert from '../../../lib/Alert.jsx'
 export default function BasicPage (props) {
   return (
     <Page>
-
       <Section title="Alerts">
         <Narrative>
-          <p>Provide contextual feedback messages for typical user actions with the handful of available and flexible alert messages.</p>
+          <p>Läs mer om hur man informera användare i olika situationer under <a href="/style/guidelines/information">Riktlinjer information</a>. </p>
         </Narrative>
+      </Section>
+      <Section title="Information_Alert">
         <Stage>
+          <h2>Information Alert</h2>
+          <Scene>
+            <Alert color="info">
+              <p>Some cool text in a <b>info</b> alert box!</p>
+            </Alert>
+            <Code>
+{`<Alert color="info">
+  <p>Some cool text in a <b>info</b> alert box!</p>
+  </Alert>`}
+            </Code>
+          </Scene>
+        </Stage>
+        <UXComment>
+          <h4>Användning</h4>
+          <ul>
+            <li>
+              För att informera användaren på en sida när användaren inte efterfrågat informationen.
+            </li>
+            <li>
+              Se .. hur man informerar när användaren har efterfrågat informationen...länk till modal
+            </li>
+            <li>
+              Placera
+              <ul>
+                <li>
+                  Överst på sidan (vilket kan innebära att man behöver "refrecha" sidan)
+                </li>
+                <li>
+                 Eller när det behövs, kila in den på rätt plats på sidan (t ex för information i schemat som gäller visst datum).
+                </li>
+              </ul>
+            </li>
+          </ul>
+            <h4>Tänk på att</h4>
+            <ul>
+              <li>
+                Se <a href="/style/guidelines/information#general_guidelines_information">Generella riktlinjer information</a>.
+              </li>
+            </ul>
+          </UXComment> 
+      </Section>
+      
+      <Section title="Error_Alert">
+        <Stage>
+          <h2>Error Alert</h2>
+          <Scene>
+            <Alert color="danger">
+              <p>Some cool text in a <b>danger</b> alert box!</p>
+            </Alert>
+            <Code>
+{`<Alert color="danger">
+  <p>Some cool text in a <b>danger</b> alert box!</p>
+</Alert>`}
+            </Code>
+          </Scene>
+        </Stage>
+        <UXComment> 
+          <h4>Användning</h4>
+          <ul>
+            <li>
+              Först fundera på om det går att hjälpa användaren att undvika felet (länk till Tillgänglig kod).
+            </li>
+            <li>
+              Om felet inte kommaer att gå att undvika: använd "Error alert" för att informera om att något har gått fel.
+            </li>
+            <li>
+              Placera
+            <ul>
+              <li>
+                  Överst på sidan (vilket kan innebära att man behöver "refrecha" sidan).
+              </li>
+              <li>
+                  För flera fel på en sida (t ex formulär), komplettera med att också markera de fält som är felaktiga med anslutande röd text.
+              </li>
+            </ul>
+            </li>
+          </ul>
+          <h4>Tänk på att</h4>
+          <ul>
+            <li>
+              Ge tydliga instruktioner som:
+            <ul>
+              <li>
+                Hjälper användaren att hitta var problemet är.
+              </li>
+              <li>
+                Förklarar vad som hänt, varför det blivit fel.
+              </li>
+              <li>
+                Föreslår hur användaren ska göra för att åtgärda felet.
+              </li>
+            </ul>
+            </li>
+            <li>
+              Se <a href="/style/guidelines/information#general_guidelines_information">Generella riktlinjer information</a>.
+            </li>
+          </ul>
+         </UXComment> 
+      </Section>  
+
+      <Section title="Success_Alert">
+        <Stage>
+          <h2>Success Alert</h2>
+          <UXComment>
+              <h4>Användning</h4>
+                <ul>
+                  <li>
+                    För att bekräfta att användaren lyckats utföra en tidigare handling.
+                  </li>
+                  <li>
+                    För längre meddelanden där informationen behöver läsas mer nogrannt.
+                  </li>
+                  <li>
+                    Placera: överst på sidan (vilket kan innebära att man behöver "refrecha" sidan)
+                  </li>
+                </ul>
+                <h4>Tänk på att</h4>
+                <ul>
+                  <li>
+                    Se <a href="/style/guidelines/information#general_guidelines_information">Generella riktlinjer information</a>.
+                  </li>
+                </ul>
+          </UXComment>
+          <h3>With close button</h3>
+          <UXComment>
+           <h4>Användning</h4>
+                <ul>
+                  <li>
+                    För längre meddelanden där informationen behöver läsas mer nogrannt.
+                  </li>
+                </ul>
+          </UXComment>
+            <Scene>
+            <Alert color="success">
+              <p>Some cool text in a <b>success</b> alert box!</p>
+            </Alert>
+            <Code>
+{`<Alert color="success">
+  <p>Some cool text in a <b>success</b> alert box!</p>
+  </Alert>`}
+            </Code>
+          </Scene>
+        </Stage>
+      <h3>Disappears after 5 seconds</h3>
+        <UXComment>
+           <h4>Användning</h4>
+                <ul>
+                  <li>
+                    För korta meddelanden där informationen inte behöver läsas. Exempel: "Saved"
+                  </li>
+                </ul>
+          </UXComment>
+      </Section> 
+      
+      <Section> 
+      <h2>Olika andra varianter av alerts - ha med?</h2>
+      <Stage>
           <Scene>
             <Alert color="success">
               <p>Some cool text in a <b>success</b> alert box!</p>
@@ -64,102 +222,7 @@ export default function BasicPage (props) {
             </Code>  
           </Scene>
         </Stage>
-      </Section>
-      
-      <Section title="Information_Alert">
-        <Narrative>
-          <p>Provide contextual feedback messages for typical user actions with the handful of available and flexible alert messages.</p>
-        </Narrative>
-        <Stage>
-          <h2>Information Alert</h2>
-          <Scene>
-            <Alert color="info">
-              <p>Some cool text in a <b>info</b> alert box!</p>
-            </Alert>
-            <Code>
-{`<Alert color="info">
-  <p>Some cool text in a <b>info</b> alert box!</p>
-  </Alert>`}
-            </Code>
-          </Scene>
-        </Stage>
-      </Section>
-      
-      <Section title="Error_Alert">
-        <Narrative>
-          <p>Provide contextual feedback messages for typical user actions with the handful of available and flexible alert messages.</p>
-        </Narrative>
-        <Stage>
-          <h2>Error Alert</h2>
-          <Scene>
-            <Alert color="danger">
-              <p>Some cool text in a <b>danger</b> alert box!</p>
-            </Alert>
-            <Code>
-{`<Alert color="danger">
-  <p>Some cool text in a <b>danger</b> alert box!</p>
-</Alert>`}
-            </Code>
-          </Scene>
-        </Stage>
-      </Section>  
-
-      <Section title="Success_Alert">
-        <Narrative>
-          <p>Provide contextual feedback messages for typical user actions with the handful of available and flexible alert messages.</p>
-        </Narrative>
-        <Stage>
-          <h2>Success Alert</h2>
-          <UXComment>
-              <h4>Användning</h4>
-                <ul>
-                  <li>
-                    För att bekräfta att användaren lyckats utföra en tidigare handling.
-                  </li>
-                  <li>
-                    För längre meddelanden där informationen behöver läsas mer nogrannt.
-                  </li>
-                </ul>
-                <h4>Tänk på att</h4>
-                <ul>
-                   <li>
-                    Placera överst på sidan och se till att fokus är där.
-                  </li>
-                  <li>
-                    Se <a href="#generellariktlinjerinformation">Generella riktlinjer information</a> nedan.
-                  </li>
-                </ul>
-          </UXComment>
-          <h3>With close button</h3>
-          <UXComment>
-           <h4>Användning</h4>
-                <ul>
-                  <li>
-                    För längre meddelanden där informationen behöver läsas mer nogrannt.
-                  </li>
-                </ul>
-          </UXComment>
-            <Scene>
-            <Alert color="success">
-              <p>Some cool text in a <b>success</b> alert box!</p>
-            </Alert>
-            <Code>
-{`<Alert color="success">
-  <p>Some cool text in a <b>success</b> alert box!</p>
-  </Alert>`}
-            </Code>
-          </Scene>
-        </Stage>
-      <h3>Disappears after 5 seconds</h3>
-        <UXComment>
-           <h4>Användning</h4>
-                <ul>
-                  <li>
-                    För korta meddelanden där informationen inte behöver läsas. Exempel: "Saved"
-                  </li>
-                </ul>
-          </UXComment>
-      </Section> 
+     </Section>   
     </Page>
   )
 }
