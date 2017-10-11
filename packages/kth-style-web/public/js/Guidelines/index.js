346e3e476f04cd845e954fb84b0d7438e7b706e1
asalindstrom <asahu@kth.se>
2017-10-11 20:06:07 +0200

    Updated wit page for menues

--- packages/kth-style-web/public/js/Guidelines/index.js
+++ packages/kth-style-web/public/js/Guidelines/index.js
@@ -1,11 +1,13 @@
 import StartPage from './StartPage.jsx'
 import InformationPage from './InformationPage.jsx'
 import ButtonOrLinkPage from './ButtonOrLinkPage.jsx'
+import MenuPage from './MenuPage.jsx'
 
 const pages = [
   { path: "", title: "Guidelines", header: "h2", component: StartPage},
   { path: "/information", title: "Information", component: InformationPage},
-  { path: "/buttonorlink", title: "Knappar och länkar", component: ButtonOrLinkPage}
+  { path: "/buttonorlink", title: "Knappar och länkar", component: ButtonOrLinkPage},
+  { path: "/menu", title: "Menyer", component: MenuPage}
 ]
 
 export default pages
