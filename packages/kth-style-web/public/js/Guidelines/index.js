40dc37067686e3fed4c19f2cdae4d1a26a28a1f7
asalindstrom <asahu@kth.se>
2017-10-12 08:25:11 +0200

    Update with pages for usability and accessability

--- packages/kth-style-web/public/js/Guidelines/index.js
+++ packages/kth-style-web/public/js/Guidelines/index.js
@@ -1,10 +1,14 @@
 import StartPage from './StartPage.jsx'
+import UsabilityPage from './UsabilityPage.jsx'
+import AccessabilityPage from './AccessabilityPage.jsx'
 import InformationPage from './InformationPage.jsx'
 import ButtonOrLinkPage from './ButtonOrLinkPage.jsx'
 import MenuPage from './MenuPage.jsx'
 
 const pages = [
   { path: "", title: "Guidelines", header: "h2", component: StartPage},
+  { path: "/usability", title: "Användbarhet", component: UsabilityPage},
+  { path: "/accessability", title: "Tillgänglighet", component: AccessabilityPage},
   { path: "/information", title: "Information", component: InformationPage},
   { path: "/buttonorlink", title: "Knappar och länkar", component: ButtonOrLinkPage},
   { path: "/menu", title: "Menyer", component: MenuPage}
