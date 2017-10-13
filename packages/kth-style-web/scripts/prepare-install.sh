5079777b37428482918370a6e3406c282137a7fa
jhsware <sebastian@urbantalk.se>
2017-10-13 17:23:31 +0200

    More debug output

--- packages/kth-style-web/scripts/prepare-install.sh
+++ packages/kth-style-web/scripts/prepare-install.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 echo "*** Current working directory:"
 echo $(pwd)
+echo "*** Content of kth-style root:"
+echo $(ls -alh ../../)
+echo "*** **************************"
+echo "*** Content of kth-style root/tmp:"
+echo $(ls -alh ../../tmp/)
+echo "*** **************************"
+
 if [ ! -d ./build ]; then
   echo "*** Creating ./build in kth-style-web"
   mkdir -p ./build;
