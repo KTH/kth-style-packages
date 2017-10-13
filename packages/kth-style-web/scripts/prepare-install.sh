24b83fda734745ec8c982225757732ff6bf8f641
jhsware <sebastian@urbantalk.se>
2017-10-13 17:12:09 +0200

    Debug info

--- packages/kth-style-web/scripts/prepare-install.sh
+++ packages/kth-style-web/scripts/prepare-install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+echo "*** Current working directory:"
+echo $(pwd)
 if [ ! -d ./build ]; then
+  echo "*** Creating ./build in kth-style-web"
   mkdir -p ./build;
 fi
 
+echo "*** Copy local packages to ./build"
 cp ../../tmp/build/* ./build/
-echo "*** Local packages copied to ./build"
