bb1bb077aa24db62c29423770e8bf5820b83ab8c
asalindstrom <asahu@kth.se>
2017-10-17 16:05:33 +0200

    Create ReadMorePage.jsx

---
+++ packages/kth-style-web/public/js/Guidelines/ReadMorePage.jsx
@@ -0,0 +1,8 @@
+import { Page, Section, Narrative } from 'kth-style-inferno-bootstrap/docs/src/components.jsx'
+
+export default function DropdownsPage (props) {
+  return (
+    <Page>
+    </Page>
+  )
+}
