096ac7c500d4ee7c5d43a9a6992c785c933e65bf
jhsware <sebastian@urbantalk.se>
2017-10-17 13:38:05 +0200

    Make sure we build the packages, the prepare script isn't run with npm v3 which we have in our containers

--- scripts/pack-for-ref.sh
+++ scripts/pack-for-ref.sh
@@ -7,23 +7,18 @@ kth-style-inferno-components"
 echo "*** Packing local packages and adding to ./build"
 echo $(which npm)
 if [ ! -d ./build ]; then
-  mkdir -p ./build;
+  mkdir -p build;
 fi
 
-cd ./build
 for p in $PACKAGES; do
 	echo "*** Building and packing $p"
-  cd ../packages/$p
-  npm install --development
-  # npm run build
-  # Go to build folder
-  cd ../../build
-  FILE_NAME=$(npm pack ../packages/$p)
-  rm -r ../packages/$p/node_modules
+  (cd packages/$p && npm install --development && npm run build)
+  # Package to build folder
+  (cd build && FILE_NAME=$(npm pack ../packages/$p))
+  rm -r packages/$p/node_modules
 done
-cd ..
 
 echo "*** Contents of [kth-style container]/build ***"
 echo "*** These are the locally generated NPM-packages that should be used for deployment of kth-style-web"
-echo $(ls ./build)
+echo $(ls build)
 echo "*** DONE ***"
