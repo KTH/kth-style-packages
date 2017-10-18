f2a579095a53058481d402eb98845d6a97237d03
asalindstrom <asahu@kth.se>
2017-10-18 08:54:08 +0200

    Update with HeadingPAge

--- packages/kth-style-web/public/js/Guidelines/index.js
+++ packages/kth-style-web/public/js/Guidelines/index.js
@@ -1,7 +1,8 @@
 import StartPage from './StartPage.jsx'
 import UsabilityPage from './UsabilityPage.jsx'
+import AccessabilityPage from './HeadingPage.jsx'
 import StructurePage from './StructurePage.jsx'
-import AccessabilityPage from './AccessabilityPage.jsx'
+import HeadingPage from './AccessabilityPage.jsx'
 import InformationPage from './InformationPage.jsx'
 import ButtonOrLinkPage from './ButtonOrLinkPage.jsx'
 import MenuPage from './MenuPage.jsx'
@@ -12,8 +13,9 @@ import ReadMorePage from './ReadMorePage.jsx'
 const pages = [
   { path: "", title: "Guidelines", header: "h2", component: StartPage},
   { path: "/usability", title: "Användbarhet", component: UsabilityPage},
-   { path: "/structure", title: "Strukturera en sida", component: StructurePage},
   { path: "/accessability", title: "Tillgänglighet", component: AccessabilityPage},
+  { path: "/structure", title: "Strukturera en sida", component: StructurePage},
+  { path: "/structure", title: "Rubriker, brödtext", component: HeadingPage},
   { path: "/information", title: "Information/ återkoppling", component: InformationPage},
   { path: "/buttonorlink", title: "Knappar och länkar", component: ButtonOrLinkPage},
   { path: "/menu", title: "Menyer", component: MenuPage},
