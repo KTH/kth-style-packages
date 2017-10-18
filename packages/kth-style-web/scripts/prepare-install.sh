b9c67c05c0723403e22aa1db31700a5661f35b2b
jhsware <sebastian@urbantalk.se>
2017-10-18 09:49:22 +0200

    Forgot to rename the folder we are creating

--- packages/kth-style-web/scripts/prepare-install.sh
+++ packages/kth-style-web/scripts/prepare-install.sh
@@ -6,9 +6,9 @@ echo "*** Contents of kth-style root/tmp/build:"
 echo $(ls ../../tmp/build)
 echo "*** **************************"
 
-if [ ! -d ./build ]; then
-  echo "*** Creating ./build in kth-style-web"
-  mkdir -p ./build;
+if [ ! -d ./localPackages ]; then
+  echo "*** Creating ./localPackages in kth-style-web"
+  mkdir -p ./localPackages;
 fi
 
 echo "*** Copy local packages to ./localPackages"
