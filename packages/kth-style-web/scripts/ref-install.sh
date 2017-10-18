d9616d5e6c5104f7c18d885847250a23b0d52a75
jhsware <sebastian@urbantalk.se>
2017-10-18 09:51:04 +0200

    Minor

--- packages/kth-style-web/scripts/ref-install.sh
+++ packages/kth-style-web/scripts/ref-install.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+echo "*** ************************* ***"
 echo "*** INSTALLING LOCAL PACKAGES ***"
+echo "*** ************************* ***"
 for filename in ./localPackages/*.tgz; do
-    # echo "$filename"
+    echo "*** Installing: $filename"
     npm install $filename
 done
+echo "*** ***************************** ***"
 echo "*** INSTALLING PUBLISHED PACKAGES ***"
+echo "*** ***************************** ***"
 npm install --development
 # npm i ./build/$KTH_STYLE ./build/$KTH_STYLE_INFERNO_BOOTSTRAP ./build/$KTH_STYLE_INFERNO_COMPONENTS
-echo "*** DONE ***"
+echo "*** ***************************** ***"
+echo "***             DONE              ***"
+echo "*** ***************************** ***"
