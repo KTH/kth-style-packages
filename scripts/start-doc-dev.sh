7c97e7c13db64d053c4d7687fcfc71e1f1d01bd4
jhsware <sebastian@urbantalk.se>
2017-10-18 11:35:04 +0200

    Create a cli for Åsa

---
+++ scripts/start-doc-dev.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+if [ "$1" == "init" ]; then
+  echo "Initialising repos..."
+  npm install lerna
+  lerna bootstrap
+elif [ "$1" == "kth-style" ]; then
+	echo "Starting kth-style build watcher..."
+  (cd packages/kth-style && npm run build-dev)
+
+elif [ "$1" == "kth-style-inferno-bootstrap" ]; then
+	echo "Starting kth-style-inferno-bootstrap docs build watcher..."
+  (cd packages/kth-style-inferno-bootstrap && npm run build-docs-dev)
+
+elif [ "$1" == "kth-style-inferno-components" ]; then
+	echo "Starting kth-style-inferno-components docs build watcher..."
+  (cd packages/kth-style-inferno-components && npm run build-docs-dev)
+
+elif [ "$1" == "kth-style-web" ]; then
+	echo "Starting kth-style-web in development mode"
+  (cd packages/kth-style-web && npm run start-dev)
+
+else
+  echo "Usage: start-doc-dev.sh [command]"
+  echo
+  echo "Valid commands:"
+  echo
+  echo "   init                         -- initialise project for development"
+  echo "   kth-style                    -- start the gulp watcher to automatically recompile sass-files"
+  echo "   kth-style-inferno-bootstrap  -- start the automatic recompile of docs"
+  echo "   kth-style-inferno-components -- start the automatic recompile of docs"
+  echo "   kth-style-web                -- start the automatic recompile of docs and launch server"
+  echo
+fi
