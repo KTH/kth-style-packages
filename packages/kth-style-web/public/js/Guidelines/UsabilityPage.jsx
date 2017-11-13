cfdddfe8985e7d7d8eaf793115eca649ec106f19
asalindstrom <asahu@kth.se>
2017-11-13 18:10:01 +0100

    Update with a link

--- packages/kth-style-web/public/js/Guidelines/UsabilityPage.jsx
+++ packages/kth-style-web/public/js/Guidelines/UsabilityPage.jsx
@@ -3,7 +3,10 @@ import { Page, Section, Narrative } from 'kth-style-inferno-bootstrap/docs/src/c
 export default function DropdownsPage (props) {
   return (
     <Page>
-      
+      <h2 id="riktlinjer_anvandbarhet">Riktlinjer användbarhet</h2>
+      <p>
+        <a href="https://www.kth.se/social/group/kth-style-guide/page/usability-heuristics-2/">Usability Heuristics</a>
+      </p>
     </Page>
   )
 }
