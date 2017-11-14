f415557b590bc070e6c1e9e6a99e5a977f67e814
asalindstrom <asahu@kth.se>
2017-11-14 16:02:29 +0100

    Update with textPage

--- packages/kth-style-web/public/js/PageLayout/index.js
+++ packages/kth-style-web/public/js/PageLayout/index.js
@@ -1,8 +1,11 @@
 /* PageLayout */
 import StartPage from './StartPage.jsx'
+import TextPage from './TextPage.jsx'
+
 
 const pages = [
   { path: "", title: "Page Layout", header: "h2", component: StartPage},
+  { path: "/textpage", title: "Textsida", component: TextPage},
 ]
 
 export default pages
