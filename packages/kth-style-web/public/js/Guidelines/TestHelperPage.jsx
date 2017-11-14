72e7af1dafe6d83aab680fe13dad97b8a40fd98c
asalindstrom <asahu@kth.se>
2017-11-14 15:50:33 +0100

    Create TestHelperPage.jsx

---
+++ packages/kth-style-web/public/js/Guidelines/TestHelperPage.jsx
@@ -0,0 +1,9 @@
+import { Page, Section, Narrative } from 'kth-style-inferno-bootstrap/docs/src/components.jsx'
+
+export default function DropdownsPage (props) {
+  return (
+    <Page>
+      <h1>Riktlinjer test</h1>
+       </Page>
+  )
+}
