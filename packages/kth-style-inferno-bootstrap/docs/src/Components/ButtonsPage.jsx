d82e4a7ab18258bb605199f9d9d630b4afb0eb4d
asalindstrom <asahu@kth.se>
2017-11-15 10:17:54 +0100

    Update with UX comments

--- packages/kth-style-inferno-bootstrap/docs/src/Components/ButtonsPage.jsx
+++ packages/kth-style-inferno-bootstrap/docs/src/Components/ButtonsPage.jsx
@@ -22,6 +22,26 @@ export default function BasicPage (props) {
                   {`<Button color="primary">primary</Button>`}
                 </Code>
               </Scene>
+              <UXComment>
+                <h4>Användning</h4>
+                <ul>
+                    <li>
+                        För primära aktiviteter (man vill uppmuntra användaren att utföra).
+                    </li>
+                    <li>
+                        "Sök" är en special variant av denna.
+                    </li>
+                    <li>
+                        Exempel: "Ansöka".
+                    </li>
+                </ul>
+                <h4>Tänk på att</h4>
+                <ul>
+                    <li>
+                        Se <a href="#generella_riktlinjer_knappar">Generella riktlinjer för knappar</a>.
+                    </li>
+                </ul>
+              </UXComment>
             </section>
             <section id="secondary_button">
               <h3>Secondary button</h3>  
@@ -31,6 +51,34 @@ export default function BasicPage (props) {
                   {`<Button color="secondary">secondary</Button>`}
                 </Code>
               </Scene>
+             <UXComment>
+                <h4>Användning:</h4>
+                <ul>
+                    <li>
+                        För sekundära aktiviteter (man kan utföra, men uppmanas inte till det).
+                    </li>
+                    <li>
+                        Exempel:
+                        <ul>
+                            <li>
+                                "Avbryt".
+                            </li>
+                            <li>
+                                Dialoger där inget av alternativen man kan välja ska uppmuntras.
+                            </li>
+                            <li>
+                                Utfällbara meny-knappar (som använd överst på sidor där man vill få plats med flera val).
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                <h4>Tänk på att: </h4>
+                <ul>
+                    <li>
+                        Se <a href="#generella_riktlinjer_knappar">Generella riktlinjer för knappar</a>.
+                    </li>
+                </ul>
+            </UXComment> 
             </section>   
             <section id="success_button">
               <h3>Success button</h3>  
@@ -40,6 +88,32 @@ export default function BasicPage (props) {
                   {`<Button color="success">success</Button>`}
                 </Code>
               </Scene>
+              <UXComment>
+                <h4>Användning</h4>
+                <ul>
+                    <li>
+                        För att uppmana användaren att avsluta en uppgift som hen har påbörjat.
+                    </li>
+                    <li>
+                        Exempel: "Spara" och "Ok".
+                    </li>
+                </ul>
+                <h4>Tänk på att</h4>
+                <ul>
+                    <li>
+                        Komplettera med ett avbryt-alternativ (se <a href="/style/dialogs">Dialoger</a>).
+                    </li>
+                    <li>
+                        Använd inte för destruktiva aktiviteter som ex "Ta bort fil" (se <a href="/style/dialogs">Dialoger</a>).
+                    </li>
+                    <li>
+                        Var försiktig att göra knappen till default-val då slutanvändaren kan råka göra valet oavsiktligt genom ”Enter”.
+                    </li>
+                    <li>
+                        Se även <a href="#generella_riktlinjer_knappar">Generella riktlinjer för knappar</a>.
+                    </li>
+                </ul>
+              </UXComment>
             </section>
             <section id="danger_button">
               <h3>Danger button</h3>  
@@ -49,6 +123,26 @@ export default function BasicPage (props) {
                   {`<Button color="danger">danger</Button>`}
                 </Code>
               </Scene>
+              <UXComment>
+                <h4>Användning</h4>
+                <ul>
+                    <li>
+                        För aktiviteter som är destruktiva.
+                    </li>
+                    <li>
+                        Exempel: "Ta bort".
+                    </li>
+                </ul>
+                <h4>Tänk på att</h4>
+                <ul>
+                    <li>
+                        "Delete" ska följas av dialog där användaren får bekräfta sitt val (se <a href="/style/dialogs">Dialoger</a>)
+                    </li>
+                    <li>
+                        Se även <a href="#generella_riktlinjer_knappar">Generella riktlinjer för knappar</a>.
+                    </li>
+                </ul>
+              </UXComment>
             </section>
             <section id="disabled_button">
               <h3>Disabled Buttons</h3>
@@ -62,6 +156,23 @@ export default function BasicPage (props) {
                   {`<Button color="primary" size="lg" disabled>Primary button</Button>`}
                 </Code>
               </Scene>
+              <UXComment>
+                <h4>Användning</h4>
+                <ul>
+                    <li>
+                        För aktiviteter som inte är möliga i ett specifikt tillstånd.
+                    </li>
+                    <li>
+                        Exempel: "Spara" när man inte har fyllt i alla nödvändiga uppgifter i ett formulär.
+                    </li>
+                </ul>
+                <h4>Tänk på att</h4>
+                <ul>
+                    <li>
+                        Se <a href="#generella_riktlinjer_knappar">Generella riktlinjer för knappar.</a>
+                    </li>
+                </ul>
+              </UXComment>
             </section>                  
           </Section>
         </Stage>           
@@ -75,9 +186,13 @@ export default function BasicPage (props) {
                   {`<Button color="link">link</Button>`}
                 </Code>
               </Scene>
+              <UXComment>
+              
+              </UXComment>
             </section>
             <section id="link_button">
               <h3>Link button</h3> 
+              <p>TO DO: Länka till komponent</p>
               <UXComment>
                 <h4>Användning</h4>
                 <ul>
