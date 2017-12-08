c095122a0cacd1bcc28955da8c3c5c20ccaf95ca
Elena Rakhimova <elenara@kth.se>
2017-12-08 17:32:35 +0100

    Added card with vertical navigation, need some more styling

---
+++ packages/kth-style-inferno-bootstrap/docs/src/Components/Card/NavCard.jsx
@@ -0,0 +1,74 @@
+import { Section, Stage, Scene, Code, Narrative,  } from '../../components.jsx'
+
+import Card from '../../../../lib/Card/Card.jsx'
+import CardBody from '../../../../lib/Card/CardBody.jsx'
+
+
+import Nav from '../../../../lib/Navigation/Nav.jsx'
+import NavItem from '../../../../lib/Navigation/NavItem.jsx'
+import NavLink from '../../../../lib/Navigation/NavLink.jsx'
+
+export default function () {
+  return (
+    <Section title="Vertical Navigation Card">
+      <Narrative>
+        <p>Use a card to display veritcal navigation in an engaging and concise manner.</p>  
+      </Narrative>  
+      <Stage>
+        <Scene>
+            <Card>
+                <CardBody>
+                    <div className="course-card big-icon">
+                        <span>
+                            <h4>Algebra och geometri (SF1624)</h4>
+                            <Nav  vertical>
+                                <NavItem>
+                                    <NavLink href="http://localdev.kth.se:8000/social/course/EQ2220/">Kursinformation</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink href="https://kth.test.instructure.com/courses/2340" className="icon-canvas">Material HT17-9</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink href="https://kth.test.instructure.com/courses/2340" className="icon-canvas">Material HT17-9</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink href="https://kth.test.instructure.com/courses/2340" className="icon-canvas">Material HT17-9</NavLink>
+                                </NavItem>
+                            </Nav>
+                        </span>
+                        <i className="course-pie-grey-50"></i>
+                    </div>
+                </CardBody>
+            </Card>
+
+            <Code>
+{`<Card>
+<CardBody>
+    <div className="course-card big-icon">
+        <span>
+            <h4>Algebra och geometri (SF1624)</h4>
+            <Nav  vertical className="white-m">
+                <NavItem>
+                    <NavLink href="http://localdev.kth.se:8000/social/course/EQ2220/">Kursinformation</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="https://kth.test.instructure.com/courses/2340" className="icon-canvas">Material HT17-9</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="https://kth.test.instructure.com/courses/2340" className="icon-canvas">Material HT17-9</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="https://kth.test.instructure.com/courses/2340" className="icon-canvas">Material HT17-9</NavLink>
+                </NavItem>
+            </Nav>
+        </span>
+        <i className="course-pie-grey-50"></i>
+    </div>
+</CardBody>
+</Card>`}
+          </Code>  
+        </Scene>
+      </Stage>
+    </Section>
+  )
+}
