8c8193c0489d5eb67eb0ce1f6699b22231969de0
asalindstrom <asahu@kth.se>
2017-12-20 19:56:40 +0100

    Update index.js

--- packages/kth-style-web/public/js/PageLayout/index.js
+++ packages/kth-style-web/public/js/PageLayout/index.js
@@ -1,11 +1,13 @@
 /* PageLayout */
 import StartPage from './StartPage.jsx'
 import TextPage from './TextPage.jsx'
+import FormPage from './FormPage.jsx'
 
 
 const pages = [
   { path: "", title: "Page Layout", header: "h2", component: StartPage},
   { path: "/textpage", title: "Textsida", component: TextPage},
+  { path: "/formpage", title: "Formulär", component: FormPage},
 ]
 
 export default pages
