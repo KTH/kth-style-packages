e96a3775922ff7ae5cc0676c91a529d73a8005d2
asalindstrom <asahu@kth.se>
2017-12-20 19:56:27 +0100

    Create FormPage.jsx

---
+++ packages/kth-style-web/public/js/PageLayout/FormPage.jsx
@@ -0,0 +1,9 @@
+import { Page, Section, Narrative } from 'kth-style-inferno-bootstrap/docs/src/components.jsx'
+
+export default function DropdownsPage (props) {
+  return (
+    <Page>
+    </Page>
+    )
+  }
+    
