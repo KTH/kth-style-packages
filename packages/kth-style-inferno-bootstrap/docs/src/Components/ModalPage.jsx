c32064fc70c8916e6c1d89ef39f0289b7f31de82
asalindstrom <asahu@kth.se>
2017-12-21 11:29:15 +0100

    Update ModalPage.jsx

--- packages/kth-style-inferno-bootstrap/docs/src/Components/ModalPage.jsx
+++ packages/kth-style-inferno-bootstrap/docs/src/Components/ModalPage.jsx
@@ -18,27 +18,6 @@ export default function BasicPage (props) {
       <p>Use modals to present essential information that takes over the entire window.</p>  
     </Narrative>  
 
-     <Stage>
-      <h4>Efterfrågad information</h4>
-      <UXComment>
-        <p> TO DO: Lägg till komponet för informera när klickat på i-ikon </p>
-        
-        <h4>Användning</h4>
-        <ul>
-          <li>
-            För att informera användaren när denne efterfrågat informationen (t ex hjälp-information)
-          </li>
-        </ul>
-        <h4>Tänk på att</h4>
-        <ul>
-          <li>
-            Se <a href="#generellariktlinjerinformation">Generella riktlinjer information</a> nedan.
-          </li>
-        </ul>
-      </UXComment>
-    </Stage>
-
-
     <Stage>
       <Scene>
 
@@ -164,9 +143,25 @@ export default function BasicPage (props) {
       </Scene>
     </Stage>
   </Section>
-
-
-
+      
+  <Stage>
+    <h3>Information modal</h3>
+    <UXComment>
+      <p> TO DO: Lägg till komponet för informera när klickat på i-ikon </p>
+      <h4>Användning</h4>
+      <ul>
+        <li>
+          För att informera användaren när denne efterfrågat informationen (t ex hjälp-information)
+        </li>
+      </ul>
+      <h4>Tänk på att</h4>
+      <ul>
+        <li>
+          Se <a href="#generellariktlinjerinformation">Generella riktlinjer information</a> nedan.
+        </li>
+      </ul>
+    </UXComment>
+  </Stage>
 
     </Page>
   )
