5bb390790edb262b6ecbc0c8180514b2d41b94d9
asalindstrom <asahu@kth.se>
2018-01-03 14:38:04 +0100

    Update ButtonsPage.jsx

--- packages/kth-style-inferno-bootstrap/docs/src/Components/ButtonsPage.jsx
+++ packages/kth-style-inferno-bootstrap/docs/src/Components/ButtonsPage.jsx
@@ -8,7 +8,7 @@ export default function BasicPage (props) {
   return (
     <Page>
       <h1>Knappar och länkar</h1>
-      <p>Läs först <a href="/style/guidelines/buttonorlink">riktlinjer för knappar och länkar</a>.</p>    
+      <p>Läs först <a href="/style/guidelines/buttonorlink">riktlinjer för knappar och länkar</a> där du får hjälp att välja rätt typ av komponent.</p>    
 
        
       <h2>Knappar</h2>
