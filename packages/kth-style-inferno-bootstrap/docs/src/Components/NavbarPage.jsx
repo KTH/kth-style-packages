45deee27ff1df757e42c57e8906bd627cae8b94d
asalindstrom <asahu@kth.se>
2018-01-03 20:04:56 +0100

    Update NavbarPage.jsx

--- packages/kth-style-inferno-bootstrap/docs/src/Components/NavbarPage.jsx
+++ packages/kth-style-inferno-bootstrap/docs/src/Components/NavbarPage.jsx
@@ -4,12 +4,24 @@ export default function NavbarPage (props) {
   return (
     <Page>
 
-      <Section title="TODO">
-        <Narrative>
+     <h1>Navbar</h1>
+        <p>
           TODO, Compare with <a href="https://getbootstrap.com/docs/4.0/components/navbar/">Bootstrap</a>.
-        </Narrative>
-      </Section>
-
+        </p>
+        <p>Läs först <a href="/style/guidelines/menu">riktlinjer meny</a> där du får hjälp att välja rätt typ av komponent.</p>
+  
+        <Section title="Menu Link button">
+          <Stage>
+              <UXComment>
+                <h4>Användning</h4>
+                <ul>
+                  <li>
+                    Lista menyalternativ i form av länkar under en meny-knapp
+                  </li>
+                </ul>
+              </UXComment>
+            </Stage>
+         </Section>   
     </Page>
   )
 }
