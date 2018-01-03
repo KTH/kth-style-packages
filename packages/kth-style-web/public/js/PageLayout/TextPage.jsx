a32213cbf44be292ade0d170d46a66efaecd059d
asalindstrom <asahu@kth.se>
2018-01-03 16:07:04 +0100

    Update TextPage.jsx

--- packages/kth-style-web/public/js/PageLayout/TextPage.jsx
+++ packages/kth-style-web/public/js/PageLayout/TextPage.jsx
@@ -3,23 +3,20 @@ import { Page, Section, Narrative } from 'kth-style-inferno-bootstrap/docs/src/c
 export default function DropdownsPage (props) {
   return (
     <Page>
-      <h1>Exempelsida rubriker och brödtext (H1)</h1>
+      <h1>Exempelsida rubriker och brödtext</h1>
       <h2>Verksamheten inom miljö och hållbar utveckling på KTH</h2>
-      <p>Forskning inom hållbar utveckling fordrar oftast samarbete mellan forskare med olika kompetenser och mellan forskare och företrädare för näringsliv och samhälle. Systemperspektiv är ofta nödvändigt och forskningen bedrivs därför ofta i nätverk och grupper som sträcker sig både inom och utanför KTH.
-</p>
-      <h2>Samarbetet i forskningen(H2)</h2>
+      <p>Forskning inom hållbar utveckling fordrar oftast samarbete mellan forskare med olika kompetenser och mellan forskare och företrädare för näringsliv och samhälle. Systemperspektiv är ofta nödvändigt och forskningen bedrivs därför ofta i nätverk och grupper som sträcker sig både inom och utanför KTH.</p>
+      <h2>Samarbetet i forskningen</h2>
       
-      <h3>Öka samarbetet på KTH(H3)</h3>
+      <h3>Öka samarbetet på KTH</h3>
       <p>För att öka samarbetet i KTH:s forskning och forskarutbildning för miljö och hållbar utveckling finns bland annat KTH:s strategiska forskningsplattformar. De nätverk som KTH:s hållbarhetsforskare deltar i är globala och innefattar hundratals forskargrupper runt om i världen från såväl ledande universitet som industriföretag.</p>
       <h3>Miljö och hållbar utveckling som huvudperspektiv</h3>
       <p>Flera enheter inom KTH har miljö och hållbar utveckling som huvudperspektiv. Där bedrivs bland annat ledande forskning om metoder och verktyg för att bedöma och utvärdera effekter av produkter och beslutsprocesser. Andra grupper arbetar med att bättre förstå samverkan mellan tekniska system och naturen. Ytterligare grupper studerar globala samband och klimateffekter i våra energiförsörjningssytem.</p>
-      <h2>Grupperingar inom KTH(H2)</h2>
-      <h3>En viktig aspekt inom forskning (H3)</h3>
+      <h2>Grupperingar inom KTH</h2>
+      <h3>En viktig aspekt inom forskning</h3>
       <h4>Förbättra miljöprestanda i industriella processer</h4>
       <p>Ytterligare många grupper har hållbar utveckling som en viktig aspekt i sin forskning. Det kan handla om att förbättra miljöprestanda i industriella processer, fordon, byggnader eller försörjningssystem. Men det finns även de på KTH som forskar om miljö och hållbar utveckling från ett historiskt och filosofiskt perspektiv. KTH har exempelvis Sveriges ende.</p>
-      <p>Stora, globala utmaningar kräver multidisciplinära angreppssätt, inte bara för själva forskningen, utan även för att resultaten skall vara anpassade till de slutliga användarna.
-
-    
+      <p>Stora, globala utmaningar kräver multidisciplinära angreppssätt, inte bara för själva forskningen, utan även för att resultaten skall vara anpassade till de slutliga användarna.</p>
     </Page>
   )
 }
