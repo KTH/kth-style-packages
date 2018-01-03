ac4aaf5f8747f294c4bd44c917267adeb42dddff
asalindstrom <asahu@kth.se>
2018-01-03 14:03:39 +0100

    Update CollapsePage.jsx

--- packages/kth-style-inferno-bootstrap/docs/src/Components/CollapsePage.jsx
+++ packages/kth-style-inferno-bootstrap/docs/src/Components/CollapsePage.jsx
@@ -8,135 +8,220 @@ export default function BasicPage (props) {
   return (
     <Page>
       <h1>Collapse</h1>
-      
-    
       <p>Läs först <a href="/style/guidelines/structure">riktlinjer för struktur, layout på sida</a> där du får överblick hur du kan strukturera en sida och få hjälp att välja rätt typ av komponent.</p>  
-  
-    <Stage>
-      <Section title="Vit-utfällbar-rubriklist">
-      <h3>Default open</h3>  
-      <Scene>
-        <ToggleCollapse defaultOpen={true} />
-      </Scene>
-      <h3>Default closed</h3>
-      <Scene>
-        <ToggleCollapse defaultOpen={false} />
-      </Scene>
-      <h3>Vit-utfällbar-rubriklist</h3>
-        <p>TO DO: Visa komponent</p>
-        <UXComment>
-          <h4>Användning</h4>
-          <ul>
-            <li>
-              För att dölja innehåll under en rubriklist som inte tar för mycket fokus på sidan. Innehållet är inte kritiskkt.
-            </li>
-            <li>
-              Exempel: ?
-            </li>
-         </ul>
-         <h4>Tänk på att</h4>
-         <ul>
-           <li>
-             Försäkra dig om att användaren ser den vita rubriklisten (se <a href="/style/testhelper#testa_anvandbarhet">Testa användbarhet</a>). Om inte överväg att använda Blå-utfällbar-rubriklist.
-           </li>
-           <li>
-             Se <a href="/style/guidelines/structure/#generella_riktlinjer_rubriklist">Generella riktlinjer rubriklist</a>.
-           </li>
-        </ul>
-      </UXComment>
-      <h3>Blå-utfällbar-rubriklist</h3>
-      <p>TO DO: Visa komponent</p>
-      <UXComment>
-        <h4>Användning</h4>
-        <ul>
-          <li>
-            För att dölja mer kritiskt innehåll under en rubriklist som syns.
-          </li>
-          <li>
-            Exempel: ?
-          </li>
-        </ul>
-        <h4>Tänk på att</h4>
-        <ul>
-          <li>
-            Se <a href="/style/guidelines/structure/#generella_riktlinjer_rubriklist">Generella riktlinjer rubriklist</a>.
-          </li>
-        </ul>
-      </UXComment>  
-      <h3>Example code:</h3>
-      <Scene>
-        <Code>
-{`class ToggleCollapse extends Component {
-  constructor(props) {
-    super(props)
+      
+      <Stage>
+        <Section title="Vit-utfällbar-rubriklist">
+          <h3>Default open</h3>  
+          <Scene>
+            <ToggleCollapse defaultOpen={true} />
+          </Scene>
+          <h3>Default closed</h3>
+          <Scene>
+            <ToggleCollapse defaultOpen={false} />
+          </Scene>
+            <p>TO DO: Visa komponent</p>
+            <UXComment>
+              <h4>Användning</h4>
+              <ul>
+                <li>
+                  För att dölja innehåll under en rubriklist som inte tar för mycket fokus på sidan. Innehållet är inte kritiskkt.
+                </li>
+                <li>
+                  Exempel: ?
+                </li>
+             </ul>
+             <h4>Tänk på att</h4>
+             <ul>
+               <li>
+                 Försäkra dig om att användaren ser den vita rubriklisten (se <a href="/style/testhelper#testa_anvandbarhet">Testa användbarhet</a>). Om inte överväg att använda Blå-utfällbar-rubriklist.
+               </li>
+               <li>
+                 Se <a href="/style/guidelines/structure/#generella_riktlinjer_rubriklist">Generella riktlinjer rubriklist</a>.
+               </li>
+            </ul>
+          </UXComment>
 
-    this.state = {
-      isOpen: false
+          <h3>Example code:</h3>
+          <Scene>
+            <Code>
+    {`class ToggleCollapse extends Component {
+      constructor(props) {
+        super(props)
+
+        this.state = {
+          isOpen: false
+        }
+
+        this.doToggle = this.doToggle.bind(this)
+      }
+
+      doToggle() {
+        this.setState({
+          isOpen: !this.state.isOpen
+        })
+      }
+
+      render() {
+        return (
+          <div>
+            <Button onClick={this.doToggle}>Do toggle!</Button>
+            <Collapse className="ExampleCollapseContainer" isOpen={this.state.isOpen}>
+              <p>Some cool content</p>
+            </Collapse>  
+          </div>  
+        )
+      }
+    }`}
+            </Code>
+          </Scene>
+
+          <h3>CSS for collapse transition:</h3>
+          <Scene>
+            <Code language="css">
+    {`.ExampleCollapseContainer {
+      background-color: aliceblue;
+      padding: 1rem;
     }
 
-    this.doToggle = this.doToggle.bind(this)
-  }
+    .CollapseAnimation-leave {
+    }
 
-  doToggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    })
-  }
+    .CollapseAnimation-leave-active {
+      overflow: hidden;
+      transition: all 0.3s ease-in;
+    }
 
-  render() {
-    return (
-      <div>
-        <Button onClick={this.doToggle}>Do toggle!</Button>
-        <Collapse className="ExampleCollapseContainer" isOpen={this.state.isOpen}>
-          <p>Some cool content</p>
-        </Collapse>  
-      </div>  
-    )
-  }
-}`}
-        </Code>
-      </Scene>
+    .CollapseAnimation-leave-end {
+      height: 0!important;
+      padding-top: 0;
+      padding-bottom: 0;
+    }
 
-      <h3>CSS for collapse transition:</h3>
-      <Scene>
-        <Code language="css">
-{`.ExampleCollapseContainer {
-  background-color: aliceblue;
-  padding: 1rem;
-}
+    .CollapseAnimation-enter {
+      height: 0!important;
+      padding-top: 0;
+      padding-bottom: 0;
+    }
 
-.CollapseAnimation-leave {
-}
+    .CollapseAnimation-enter-active {
+      transition: all 0.3s ease-in;
+      overflow: hidden;
+    }
 
-.CollapseAnimation-leave-active {
-  overflow: hidden;
-  transition: all 0.3s ease-in;
-}
+    .CollapseAnimation-enter-end {
+    }
+        `}  
+                </Code>
+              </Scene>
+        </Section>
+       </Stage>
+  
+       <Stage>
+         <Section title="Blå utfällbar rubriklist">
+            <h3>Default open</h3>  
+            <Scene>
+              <ToggleCollapse defaultOpen={true} />
+            </Scene>
+            <h3>Default closed</h3>
+            <Scene>
+              <ToggleCollapse defaultOpen={false} />
+            </Scene>
 
-.CollapseAnimation-leave-end {
-  height: 0!important;
-  padding-top: 0;
-  padding-bottom: 0;
-}
+            <p>TO DO: Visa komponent</p>
+            <UXComment>
+              <h4>Användning</h4>
+              <ul>
+                <li>
+                  För att dölja mer kritiskt innehåll under en rubriklist som syns.
+                </li>
+                <li>
+                  Exempel: ?
+                </li>
+              </ul>
+              <h4>Tänk på att</h4>
+              <ul>
+                <li>
+                  Se <a href="/style/guidelines/structure/#generella_riktlinjer_rubriklist">Generella riktlinjer rubriklist</a>.
+                </li>
+              </ul>
+            </UXComment>  
+            <h3>Example code:</h3>
+            <Scene>
+              <Code>
+      {`class ToggleCollapse extends Component {
+        constructor(props) {
+          super(props)
 
-.CollapseAnimation-enter {
-  height: 0!important;
-  padding-top: 0;
-  padding-bottom: 0;
-}
+          this.state = {
+            isOpen: false
+          }
 
-.CollapseAnimation-enter-active {
-  transition: all 0.3s ease-in;
-  overflow: hidden;
-}
+          this.doToggle = this.doToggle.bind(this)
+        }
 
-.CollapseAnimation-enter-end {
-}
-`}  
-        </Code>
-      </Scene>
-    </Section>
-   </Stage>
+        doToggle() {
+          this.setState({
+            isOpen: !this.state.isOpen
+          })
+        }
+
+        render() {
+          return (
+            <div>
+              <Button onClick={this.doToggle}>Do toggle!</Button>
+              <Collapse className="ExampleCollapseContainer" isOpen={this.state.isOpen}>
+                <p>Some cool content</p>
+              </Collapse>  
+            </div>  
+          )
+        }
+      }`}
+              </Code>
+            </Scene>
+
+            <h3>CSS for collapse transition:</h3>
+            <Scene>
+              <Code language="css">
+      {`.ExampleCollapseContainer {
+        background-color: aliceblue;
+        padding: 1rem;
+      }
+
+      .CollapseAnimation-leave {
+      }
+
+      .CollapseAnimation-leave-active {
+        overflow: hidden;
+        transition: all 0.3s ease-in;
+      }
+
+      .CollapseAnimation-leave-end {
+        height: 0!important;
+        padding-top: 0;
+        padding-bottom: 0;
+      }
+
+      .CollapseAnimation-enter {
+        height: 0!important;
+        padding-top: 0;
+        padding-bottom: 0;
+      }
+
+      .CollapseAnimation-enter-active {
+        transition: all 0.3s ease-in;
+        overflow: hidden;
+      }
+
+      .CollapseAnimation-enter-end {
+      }
+      `}  
+              </Code>
+            </Scene>
+         </Section>
+       </Stage> 
+  
+  
     </Page>
   )
 }
