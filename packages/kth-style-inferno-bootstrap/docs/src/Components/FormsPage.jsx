401fda8cf1de1f3b9ed30c7d74f8ae5be380b478
asalindstrom <asahu@kth.se>
2018-01-04 10:33:25 +0100

    Update FormsPage.jsx

--- packages/kth-style-inferno-bootstrap/docs/src/Components/FormsPage.jsx
+++ packages/kth-style-inferno-bootstrap/docs/src/Components/FormsPage.jsx
@@ -11,36 +11,143 @@ export default function BasicPage (props) {
         
       <h1>Form</h1>
       <Section title="Text input">  
+      <p> Komponent ska infogas </p>
         <Stage> 
-
+          <UXComment>
+            <h4>Användning</h4>
+            <ul>
+                <li>
+                    För indata i form av text.
+                </li>
+                <li>
+                    Exempel: se <a href="/style/page-layout/formexamplepage">formulär</a>.
+                </li>
+            </ul>
+            <h4>Tänk på att</h4>
+            <ul>
+                <li>
+                    Matcha fält till typen och storleken på data.
+                    <ul>
+                      <li>
+                        Exempel: postnummer.
+                      </li>
+                    </ul>
+                </li>
+                <li>
+                    Se <a href="#generella_riktlinjer_for_formularkomponenter">Generella riktlinjer för formulärkomponenter</a>.
+                </li>    
+            </ul>
+          </UXComment>
         </Stage>
       </Section>
       
       <Section title="Radio buttons">  
         <Stage> 
-
+          <p> Komponent ska infogas </p>
+          <UXComment>
+            <h4>Användning</h4>
+            <ul>
+                <li>
+                    För flervalssvar när bara ett val är möjligt (och alternativen är färre).
+                </li>
+                <li>
+                    Exempel: se <a href="/style/page-layout/formexamplepage">formulär</a>.
+                </li>
+            </ul>
+            <h4>Tänk på att</h4>
+            <ul>
+              <li>
+                Alternativen ska inte vara fler än att de kan presenteras överskådligt på en rad. Annars använd rullgardinsmeny.
+              </li>
+              <li>
+                Se <a href="#generella_riktlinjer_for_formularkomponenter">Generella riktlinjer för formulärkomponenter</a>.
+              </li>    
+            </ul>
+          </UXComment>
         </Stage>
       </Section>
     
       <Section title="Drop down">  
         <Stage> 
-
+            <p>Komponent ska infogas </p>
+            <UXComment>
+              <h4>Användning</h4>
+              <ul>
+                  <li>
+                      För flervalssvar när bara ett val är möjligt (och alternativen är många).
+                  </li>
+                  <li>
+                      Exempel: se <a href="/style/page-layout/formexamplepage">formulär</a>.
+                  </li>
+              </ul>
+              <h4>Tänk på att</h4>
+              <ul>
+                  <li>
+                      Undvik rullgardinsmeny när det finns färre än tre alternativ. Visa dem istället som radio-knappar (se ovan).
+                  </li>
+                  <li>
+                      Undvik rullgardinsmeny när indata är något som är välbekant för användaren och snabbare kan matas in än att välja från en lång lista
+                  </li>
+                  <li>
+                       Överväg användningsfrekvens, lista de vanligaste valen först när det är möjligt.
+                  </li>
+                  <li>
+                      Användaren ska kunna söka fram rätt alternativ i rullgardinsmenyn genom att använda bokstavstangenterna.
+                  </li>
+                  <li>
+                      Se <a href="#generella_riktlinjer_for_formularkomponenter">Generella riktlinjer för formulärkomponenter</a>.
+                  </li>    
+              </ul>          
+            </UXComment>
         </Stage>
       </Section>
       
       <Section title="Check box">  
         <Stage> 
-
+          <p> Komponent ska infogas </p>
+          <UXComment>
+            <h4>Användning</h4>
+            <ul>
+                <li>
+                    För flervalssvar när flera val är möjliga.
+                </li>
+                <li>
+                    Exempel: se <a href="/style/page-layout/formexamplepage">formulär</a>.
+                </li>
+            </ul>
+            <h4>Tänk på att</h4>
+            <ul>
+                <li>
+                    Se <a href="#generella_riktlinjer_for_formularkomponenter">Generella riktlinjer för formulärkomponenter</a>.
+                </li>    
+            </ul>
+          </UXComment>
         </Stage>
       </Section>
      
       <Section title="Date picker">  
         <Stage> 
-
+          <p> Komponent ska infogas </p>
+          <UXComment>
+            <h4>Användning</h4>
+            <ul>
+                <li>
+                  För val av datum.
+                </li>
+                <li>
+                  Exempel: se <a href="/style/page-layout/formexamplepage">formulär</a>.
+                </li>
+            </ul>
+            <h4>Tänk på att</h4>
+            <ul>
+                <li>
+                    Se <a href="#generella_riktlinjer_for_formularkomponenter">Generella riktlinjer för formulärkomponenter</a>.
+                </li>    
+            </ul>
+          </UXComment>
         </Stage>
       </Section>
       
-     
       <InputSection />
       <InputGroupSection />
       <InputWithButton />
