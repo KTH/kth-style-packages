0ead170479464ebf9d604f45fd224235dc2e38b2
asalindstrom <asahu@kth.se>
2018-01-04 11:38:36 +0100

    Update FormExamplePage.jsx

--- packages/kth-style-web/public/js/PageLayout/FormExamplePage.jsx
+++ packages/kth-style-web/public/js/PageLayout/FormExamplePage.jsx
@@ -3,6 +3,7 @@ import { Page, Section, Narrative } from 'kth-style-inferno-bootstrap/docs/src/c
 export default function DropdownsPage (props) {
   return (
     <Page>
+      <h1>Exempelsida formulär</h1>
       <p> Här ska finnas ett exempel på ett bra strukturerat formulär </p>
     </Page>
     )
