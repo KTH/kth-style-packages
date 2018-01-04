1f132b07c9d7cd7769b61e18485bec3f42d2cc74
asalindstrom <asahu@kth.se>
2018-01-04 10:17:18 +0100

    Update FormsPage.jsx

--- packages/kth-style-inferno-bootstrap/docs/src/Components/FormsPage.jsx
+++ packages/kth-style-inferno-bootstrap/docs/src/Components/FormsPage.jsx
@@ -1,4 +1,4 @@
-import { Page, Code, Section, Stage, Scene, Narrative } from '../components.jsx'
+import { Page, Code, Section, Stage, Scene, Narrative, UXComment } from '../components.jsx'
 
 import InputSection from './Forms/Input.jsx'
 import InputGroupSection from './Forms/InputGroup.jsx'
@@ -8,6 +8,39 @@ import ButtonShorthand from './Forms/ButtonShorthand.jsx'
 export default function BasicPage (props) {
   return (
     <Page>
+        
+      <h1>Form</h1>
+      <Section title="Text input">  
+        <Stage> 
+
+        </Stage>
+      </Section>
+      
+      <Section title="Radio buttons">  
+        <Stage> 
+
+        </Stage>
+      </Section>
+    
+      <Section title="Drop down">  
+        <Stage> 
+
+        </Stage>
+      </Section>
+      
+      <Section title="Check box">  
+        <Stage> 
+
+        </Stage>
+      </Section>
+     
+      <Section title="Date picker">  
+        <Stage> 
+
+        </Stage>
+      </Section>
+      
+     
       <InputSection />
       <InputGroupSection />
       <InputWithButton />
