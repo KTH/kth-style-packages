72c48ae9a6526d16d8e250e727793df39208b243
asalindstrom <asahu@kth.se>
2018-01-04 11:35:19 +0100

    Update MenuPage.jsx

--- packages/kth-style-web/public/js/Guidelines/MenuPage.jsx
+++ packages/kth-style-web/public/js/Guidelines/MenuPage.jsx
@@ -4,7 +4,10 @@ import { Page, Section, Narrative } from 'kth-style-inferno-bootstrap/docs/src/c
 export default function DropdownsPage (props) {
   return (
     <Page>
-      
+      <h1>Riktlinjer meny</h1>
+          <p> Under utveckling!</p>
+          <p> Se <a href="https://www.kth.se/social/group/kth-style-guide/page/menyer/"> arbetsmaterial</a>.
+          </p>
     </Page>
   )
 }
