da4f17a76b8b8eb5dd8548bc51e038ed51e0f5af
asalindstrom <asahu@kth.se>
2018-01-04 15:50:58 +0100

    Update CollapsePage.jsx

--- packages/kth-style-inferno-bootstrap/docs/src/Components/CollapsePage.jsx
+++ packages/kth-style-inferno-bootstrap/docs/src/Components/CollapsePage.jsx
@@ -30,7 +30,7 @@ export default function BasicPage (props) {
              <h4>Tänk på att</h4>
              <ul>
                <li>
-                 Försäkra dig om att användaren ser den vita rubriklisten (se <a href="/style/testhelper#testa_anvandbarhet">Testa användbarhet</a>). Om inte överväg att använda <a href="#blue_collapse">Blue collapse</a>.
+                 Försäkra dig om att användaren ser den vita rubriklisten (se <a href="/style/guidelines/testhelper#testa_anvandbarhet">Testa användbarhet</a>). Om inte överväg att använda <a href="#blue_collapse">Blue collapse</a>.
                </li>
                <li>
                  Se <a href="/style/guidelines/structure/#generella_riktlinjer_rubriklist">Generella riktlinjer rubriklist</a>.
