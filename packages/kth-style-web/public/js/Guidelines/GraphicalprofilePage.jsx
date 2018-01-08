85ef4f39986057a784fcb69a7fa497362f6631d9
asalindstrom <asahu@kth.se>
2018-01-08 15:26:42 +0100

    Create GraphicalprofilePage.jsx

---
+++ packages/kth-style-web/public/js/Guidelines/GraphicalprofilePage.jsx
@@ -0,0 +1,13 @@
+import { Page, Section, Narrative } from 'kth-style-inferno-bootstrap/docs/src/components.jsx'
+
+export default function DropdownsPage (props) {
+  return (
+    <Page>
+      <h1>Anpassningar till grafisk profil</h1>
+
+<Section>
+</Section>
+    
+    </Page>
+  )
+}
