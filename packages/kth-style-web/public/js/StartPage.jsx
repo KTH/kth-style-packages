50c21c4428c5687f865d4e6b9a12bad1d2333ba9
asalindstrom <asahu@kth.se>
2018-01-08 15:44:32 +0100

    Uppdaterat avsnitt om grafisk profil

--- packages/kth-style-web/public/js/StartPage.jsx
+++ packages/kth-style-web/public/js/StartPage.jsx
@@ -103,74 +103,20 @@ export default function DropdownsPage (props) {
       <Section title="KTH's grafiska profil" id="kths_grafiska_profil">
            
             <section>
-                <h3>Bakgrund</h3>
                 <p>
-                KTH:s grafiska profil är beslutad av rektor.
+                  KTH:s grafiska profil är beslutad av rektor.
                 </p>
                 <p>
-                Profilen omfattar dock inte ett officiellt KTH-utseende på webb. Den webbform som tillämpas på centralt förvaltade webbmiljöer är en tillämpning av den grafiska profilen. Av det följer att forminslag följer grafisk profil i de fall denna är tillämplig. Hit hör val av fonter, färger, hantering av KTH:s logotyp och bruk av profilens särskilda grafiska element. 
+                  Profilen omfattar dock inte ett officiellt KTH-utseende på webb. Den webbform som tillämpas på centralt förvaltade webbmiljöer är en tillämpning av den grafiska profilen. Av det följer att forminslag följer grafisk profil i de fall denna är tillämplig. Hit hör val av fonter, färger, hantering av KTH:s logotyp och bruk av profilens särskilda grafiska element. 
                 </p>
                 <p>
-                Tillkommande forminslag och avsteg från grafisk profil sker utifrån webbspecifika behov samt krav på användbarhet och tillgänglighet (se <a href="/style/guidelines/usability">Riktlinjer för användbahet</a> och <a href="/style/guidelines/accessability">Riktlinjer för tillgänglighet</a>).
+                  Tillkommande forminslag och avsteg från grafisk profil sker utifrån webbspecifika behov samt krav på användbarhet och tillgänglighet (se <a href="/style/guidelines/usability">Riktlinjer för användbahet</a> och <a href="/style/guidelines/accessability">Riktlinjer för tillgänglighet</a>).
+                </p>
+                <p>
+                  Läs mer om hur KTH style har applicerat KTHs <a href="https:/style/guidelines/graphicalprofile">KTH:s profil färger</a>grafisk profil vad gäller färger och fonter.
                 </p>
         </section>
-        <section>
-            <h3>Färg</h3>
-            <h4>Hur vi använder färger</h4>
-            <ul>
-                <li>
-                    Vi utgår ifrån <a href="https://intra.kth.se/polopoly_fs/1.458089!/image/fargreferens_png.png">KTH:s profil färger</a> men har testat kontrast och om dålig kontrast justerat ljusheten (p g a tillgänglighet). Läs mer om kontrast under <a href="/style/guidelines/accessability/#farger_och_tillganglighet">Färger och tillgänglighet</a>.
-                </li>
-                <li>
-                    Färg ska inte vara det enda sättet att skilja på information utan kan fungera som ett bra tillägg till någon visuell markering (som inte bygger på färgskillnader). T ex använd understruket tillsammans med blå färg på länkad text. (Tillgänglighet).
-                </li>
-                <li>
-                    Då man använder färg i dekorativt syfte ska man göra detta utan att det samtidigt påverkar användbarhet och tillgänglighet. Exempel på felaktig användning:
-                    <ul>
-                        <li>
-                            Man använder interaktiva element som knappar för att dekorera i stället för att det egentligen hade varit mer passande med en länk. Färger ska inte distrahera användaren från det som egentligen är uppgiften på sidan (Användbarhet).
-                        </li>
-                        <li>
-                            Man använder färgade plattor med vit text som inte ger tillräcklig kontrast och därför försvårar läsbarhet (Tillgänglighet).
-                        </li>
-                        <li>
-                            Man hoppar över H2:an för att man inte tycker om den gröna färgen på rubriken (Tillgänglighet).
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    Man kan använda färger för att markera, koda innehåll. Tänk på att i samma sammanhang inte samtidigt använda färger i ett dekorativt syfte.
-                </li>
-            </ul>
-        <h4>Annpassning av KTHs profilfärger</h4>
-            <p>
-            Målet har varit att kth.se ska möta tillgänglighetskrav på nivå AA för WCAG 2.0 (se riktlinje 1.4, <a href="/style/guidelines/accessability/#overgripande_riktlinjer_tillganglighet">Övergripande riktlinjer tillgänglighet</a>) genom att för kth.se ändra färger, och teststorlek på de element som haft för dålig kontrast mellan bakgrund och text. En ytterligare positiv konsekvens av dessa förändringar är att kth.se kommer att ge en lugnare upplevelse vilket kommer att ge en bättre läsbarhet för alla användare.
-            </p>
-            <p>
-            Vi har inte velat göra för stora förändringar; kth.se ska fortfarande ge samma design-upplevelse som är baserat på den kth-profil som togs fram för ett antal år sen. Större förändringar än det vi föreslagit här kräver en större redesign med nya profilfärger, annan layout, etc.
-            </p>
-            <p>
-            I nuvarande förslag har vi mörkat ner den ljusa blå profilfärgen till nivå AA för stor text (se <a href="https://www.kth.se/social/group/kth-style-guide/page/farger/">Färger</a>) och ersatt den ljusa gröna med den gröna profilfärgen, vilket gör att vi inte frångår KTHs profil för mycket. Det kräver dock att vit text på färgad bakgrund har en fontstorlek på minst 14 om fet och annars 18 px. Alla element utom plattor kommer uppfylla detta.
-            </p>
-        </section>
-        <section>
-            <h3>Font</h3>
-            <h4>Titel</h4>
-            <ul>
-                <li>
-                    Georgia Regular, Georgia, garamond pro, garamond, times new roman, times, serif;
-                </li>
-            </ul>
-            <h4>Rubriker(H2-H4) samt brödtext</h4>
-            <ul>
-                <li>
-                    Serif: ...
-                </li>
-                <li>
-                    Sans serif: font-family: Arial, Helvetica Neue, helvetica, sans-serif
-                </li>
-            </ul>
-            </section>
+     
         </Section>
 
        <Section title="Teknik">
