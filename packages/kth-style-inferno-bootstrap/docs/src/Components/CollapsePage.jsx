8cd965e55ae29bdb6dcd3033c27576c8b3af9364
Pontus Nydensten <pontusn@kth.se>
2018-02-15 14:06:22 +0100

    New collapse

--- packages/kth-style-inferno-bootstrap/docs/src/Components/CollapsePage.jsx
+++ packages/kth-style-inferno-bootstrap/docs/src/Components/CollapsePage.jsx
@@ -14,9 +14,10 @@ export default function BasicPage (props) {
       <Section title="White collapse">
         <Stage>
           <Scene>
-            <ToggleCollapse defaultOpen={false} />
+            <Collapse className="ExampleCollapseContainer" isOpen={false} color="white">
+              <p>My phone number is only two digits off from a local pizzeria’s. I’ve spent my entire life getting occasional wrong-dials from people who accidentally fumbled and hit the wrong number, looking for pizza. One night, it is two am, and our phone rings. I let it go to machine, and am greeted by a rather amusing voicemail from a very drunk man called “Gordon”, who is practically begging for pizza to be delivered to his dorm room at the local college. I try to go back to sleep, but “Gordon” calls back again… and again. On the third call, I answer the phone.</p>
+            </Collapse>  
           </Scene>
-            <p>TO DO: Justera komponent (färg, pil, storlek)</p>
             <UXComment>
               <h4>Användning</h4>
               <ul>
@@ -41,85 +42,22 @@ export default function BasicPage (props) {
           <h3>Example code:</h3>
           <Scene>
             <Code>
-    {`class ToggleCollapse extends Component {
-      constructor(props) {
-        super(props)
-
-        this.state = {
-          isOpen: false
-        }
-
-        this.doToggle = this.doToggle.bind(this)
-      }
-
-      doToggle() {
-        this.setState({
-          isOpen: !this.state.isOpen
-        })
-      }
-
-      render() {
-        return (
-          <div>
-            <Button onClick={this.doToggle}>Do toggle!</Button>
-            <Collapse className="ExampleCollapseContainer" isOpen={this.state.isOpen}>
-              <p>Some cool content</p>
-            </Collapse>  
-          </div>  
-        )
-      }
-    }`}
+              {`<Collapse className="ExampleCollapseContainer" isOpen={false} color="white">
+  <p>My phone number is only two digits off from a local pizzeria’s. I’ve spent my entire life getting occasional wrong-dials from people who accidentally fumbled and hit the wrong number, looking for pizza. One night, it is two am, and our phone rings. I let it go to machine, and am greeted by a rather amusing voicemail from a very drunk man called “Gordon”, who is practically begging for pizza to be delivered to his dorm room at the local college. I try to go back to sleep, but “Gordon” calls back again… and again. On the third call, I answer the phone.</p>
+</Collapse>  
+              `}
             </Code>
           </Scene>
-
-          <h3>CSS for collapse transition:</h3>
-          <Scene>
-            <Code language="css">
-    {`.ExampleCollapseContainer {
-      background-color: aliceblue;
-      padding: 1rem;
-    }
-
-    .CollapseAnimation-leave {
-    }
-
-    .CollapseAnimation-leave-active {
-      overflow: hidden;
-      transition: all 0.3s ease-in;
-    }
-
-    .CollapseAnimation-leave-end {
-      height: 0!important;
-      padding-top: 0;
-      padding-bottom: 0;
-    }
-
-    .CollapseAnimation-enter {
-      height: 0!important;
-      padding-top: 0;
-      padding-bottom: 0;
-    }
-
-    .CollapseAnimation-enter-active {
-      transition: all 0.3s ease-in;
-      overflow: hidden;
-    }
-
-    .CollapseAnimation-enter-end {
-    }
-        `}  
-                </Code>
-              </Scene>
-            </Stage>
-          </Section>
+        </Stage>
+        </Section>
        
          <Section title="Blue collapse">
            <Stage>
             <Scene>
-              <ToggleCollapse defaultOpen={false} />
+              <Collapse className="ExampleCollapseContainer" isOpen={false} color="blue">
+                <p>My phone number is only two digits off from a local pizzeria’s. I’ve spent my entire life getting occasional wrong-dials from people who accidentally fumbled and hit the wrong number, looking for pizza. One night, it is two am, and our phone rings. I let it go to machine, and am greeted by a rather amusing voicemail from a very drunk man called “Gordon”, who is practically begging for pizza to be delivered to his dorm room at the local college. I try to go back to sleep, but “Gordon” calls back again… and again. On the third call, I answer the phone.</p>
+              </Collapse>  
             </Scene>
-
-            <p>TO DO: Justera komponent (färg, pil, storlek)</p>
             <UXComment>
               <h4>Användning</h4>
               <ul>
@@ -140,109 +78,15 @@ export default function BasicPage (props) {
             <h3>Example code:</h3>
             <Scene>
               <Code>
-      {`class ToggleCollapse extends Component {
-        constructor(props) {
-          super(props)
-
-          this.state = {
-            isOpen: false
-          }
-
-          this.doToggle = this.doToggle.bind(this)
-        }
-
-        doToggle() {
-          this.setState({
-            isOpen: !this.state.isOpen
-          })
-        }
-
-        render() {
-          return (
-            <div>
-              <Button onClick={this.doToggle}>Do toggle!</Button>
-              <Collapse className="ExampleCollapseContainer" isOpen={this.state.isOpen}>
-                <p>Some cool content</p>
-              </Collapse>  
-            </div>  
-          )
-        }
-      }`}
-              </Code>
-            </Scene>
-
-            <h3>CSS for collapse transition:</h3>
-            <Scene>
-              <Code language="css">
-      {`.ExampleCollapseContainer {
-        background-color: aliceblue;
-        padding: 1rem;
-      }
-
-      .CollapseAnimation-leave {
-      }
-
-      .CollapseAnimation-leave-active {
-        overflow: hidden;
-        transition: all 0.3s ease-in;
-      }
-
-      .CollapseAnimation-leave-end {
-        height: 0!important;
-        padding-top: 0;
-        padding-bottom: 0;
-      }
-
-      .CollapseAnimation-enter {
-        height: 0!important;
-        padding-top: 0;
-        padding-bottom: 0;
-      }
-
-      .CollapseAnimation-enter-active {
-        transition: all 0.3s ease-in;
-        overflow: hidden;
-      }
-
-      .CollapseAnimation-enter-end {
-      }
-      `}  
-              </Code>
-            </Scene>
+              {`<Collapse className="ExampleCollapseContainer" isOpen={false} color="blue">
+  <p>My phone number is only two digits off from a local pizzeria’s. I’ve spent my entire life getting occasional wrong-dials from people who accidentally fumbled and hit the wrong number, looking for pizza. One night, it is two am, and our phone rings. I let it go to machine, and am greeted by a rather amusing voicemail from a very drunk man called “Gordon”, who is practically begging for pizza to be delivered to his dorm room at the local college. I try to go back to sleep, but “Gordon” calls back again… and again. On the third call, I answer the phone.</p>
+</Collapse>  
+              `}
+            </Code>
+          </Scene>
           </Stage> 
          </Section>
-      
-  
-  
     </Page>
   )
 }
 
-class ToggleCollapse extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      isOpen: props.defaultOpen
-    }
-
-    this.doToggle = this.doToggle.bind(this)
-  }
-
-  doToggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <Button onClick={this.doToggle}>Do toggle!</Button>
-        <Collapse className="ExampleCollapseContainer" isOpen={this.state.isOpen}>
-          <p>My phone number is only two digits off from a local pizzeria’s. I’ve spent my entire life getting occasional wrong-dials from people who accidentally fumbled and hit the wrong number, looking for pizza. One night, it is two am, and our phone rings. I let it go to machine, and am greeted by a rather amusing voicemail from a very drunk man called “Gordon”, who is practically begging for pizza to be delivered to his dorm room at the local college. I try to go back to sleep, but “Gordon” calls back again… and again. On the third call, I answer the phone.</p>
-        </Collapse>  
-      </div>  
-    )
-  }
-}
--- packages/kth-style-inferno-bootstrap/lib/Collapse.jsx
+++ packages/kth-style-inferno-bootstrap/lib/Collapse.jsx
@@ -1,2 +1,148 @@
-import Collapse from 'inferno-bootstrap/lib/Collapse'
-export default Collapse
+import Component from 'inferno-component'
+import classNames from 'classnames'
+import { mapToCssModules, omit } from 'inferno-bootstrap/lib/utils'
+import { animateOnAdd, animateOnRemove } from 'inferno-animation'
+import createElement from 'inferno-create-element'
+
+// .collapse hides the content
+// .collapse.show shows the content
+// .collapsing
+
+const enterCls = {
+  start: '',
+  active: 'collapsing',
+  end: 'collapse show'
+}
+
+const leaveCls = {
+  start: 'collapse show',
+  active: 'collapsing',
+  end: 'collapse'
+}
+
+const defaultProps = {
+  isOpen: false,
+  tag: 'div',
+  onOpened: () => {},
+  onClosed: () => {}
+}
+
+const excludeProps = {'el': true, 'tag': true, 'prefix': true, children: true}
+
+class Animated extends Component {
+
+  componentDidMount () {
+    console.log('Animated mounted')
+    animateOnAdd(this, enterCls, this.props.onDidEnter)
+  }
+
+  componentWillUnmount () {
+    console.log('Animated unmounted')
+    animateOnRemove(this, leaveCls, this.props.onDidLeave)
+  }
+
+  render () {
+    const props = {}
+    const keys = Object.keys(this.props)
+    for (let i = 0; i < keys.length; i++) {
+      let tmpKey = keys[i]
+      if (!excludeProps[tmpKey]) {
+        props[tmpKey] = this.props[tmpKey]
+      }
+    }
+
+    return createElement(
+      this.props.el || this.props.tag || 'div', 
+      props, 
+      this.props.children
+    )
+  }
+}
+
+class Collapse extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      active: false
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      active: true
+    })
+  }
+
+  render() {
+    const {
+      className,
+      cssModule,
+      tag: Tag,
+      ...attributes
+    } = omit(this.props, ['isOpen', 'delay', 'onOpened', 'onClosed', 'animationPrefix']);
+  
+    const classes = mapToCssModules(classNames(
+      className, !this.state.active && 'InfernoAnimation-noAnim',
+    ), cssModule)
+
+    const prefix = this.props.animationPrefix || "CollapseAnimation"
+  
+    // If hidden
+    if (!this.props.isOpen) return null
+    
+    return (
+      <Animated el={Tag} prefix={prefix}
+        {...attributes}
+        className={classes}  
+        onDidEnter={this.props.onOpened}
+        onDidLeave={this.props.onClosed} />
+    );
+  }
+}
+
+Collapse.defaultProps = defaultProps
+
+class CollapsibleHeaderBar extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isOpen: false
+    }
+
+    this.doToggle = this.doToggle.bind(this)
+  }
+
+  doToggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+  
+  render () {
+    const classes = classNames(
+      'card',
+      'collapsible',
+      {'white': this.props.color === 'white'},
+      {'blue': (this.props.color === 'blue' || this.props.color !== 'white')},
+    )
+
+    return (
+      <div class={classes}>
+        <div class="card-header" role="tab" >
+            <h4 class="mb-0 mt-0">
+              <a onClick={this.doToggle} aria-expanded={this.state.isOpen}>Visa mer information</a>
+            </h4>
+        </div>
+        <Collapse isOpen={this.state.isOpen}>
+          <div class="card-body">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+          </div>        
+        </Collapse>
+      </div>
+    )
+  }
+}
+
+export default CollapsibleHeaderBar
