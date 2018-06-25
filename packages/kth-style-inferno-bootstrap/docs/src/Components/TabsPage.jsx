9443aed0a64a9d531936f84599610703e9d18ae5
asalindstrom <asahu@kth.se>
2018-06-25 11:10:58 +0200

    Update TabsPage.jsx

--- packages/kth-style-inferno-bootstrap/docs/src/Components/TabsPage.jsx
+++ packages/kth-style-inferno-bootstrap/docs/src/Components/TabsPage.jsx
@@ -1,1 +1,29 @@
 
+import { Page, Code, Section, Stage, Scene, Narrative, UXComment } from '../components.jsx'
+import Component from 'inferno-component'
+
+export default function BasicPage (props) {
+  return (
+    <Page>
+      <h1>Tabs</h1>
+      <p>Läs först <a href="/style/guidelines/structure">riktlinjer struktur, layout</a> där du får överblick hur du kan strukturera en sida och få hjälp att välja rätt typ av komponenter.</p>    
+
+          <Section title="Tabs">  
+            <Stage>
+              <UXComment>
+              <h4>Användning</h4>
+              <ul>
+                <li>
+                  ...
+                </li>
+                <li>
+                 ...
+                </li>
+              </ul>
+            </UXComment>           
+          </Stage> 
+         </Section>  
+               
+    </Page>
+  )
+}
